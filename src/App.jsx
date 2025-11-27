87941c23741ae46db256f3150ad243b268031946
StanaAndrei <stadey33@gmail.com>
2025-11-27 20:53:04 +0200

    cd x3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
 function App() {
+    console.log(import.meta.env.BASE_URL)
 
   return <Canvas shadows camera={{
       fov: 75, near: 0.1, far: 1000, position: [0, 3, 3]
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()], base: '/r3fss/',
+export default defineConfig(({ command }) => {
+    return {
+        plugins: [react()],
+        base: command === 'serve' ? '/' : '/r3fss/',
+    }
 })
