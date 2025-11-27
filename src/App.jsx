c5753dc8b99fd3737f2e7c9d76b1e88f8d5d794f
StanaAndrei <stadey33@gmail.com>
2025-11-27 21:11:49 +0200

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import MainContainer from "./MainContainer.jsx";
-import {Canvas} from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import {Loader, OrbitControls} from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
 function App() {
     console.log(import.meta.env.BASE_URL)
 
-  return <Canvas shadows camera={{
-      fov: 75, near: 0.1, far: 1000, position: [0, 3, 3]
-  }}>
-      <Perf />
-      <OrbitControls />
-      <MainContainer />
-  </Canvas>
+  return <>
+      <Canvas shadows camera={{
+          fov: 75, near: 0.1, far: 1000, position: [0, 3, 3]
+      }}>
+          <Perf />
+          <OrbitControls />
+          <MainContainer />
+      </Canvas>
+      <Loader />
+  </>
+
 }
 
 export default App
